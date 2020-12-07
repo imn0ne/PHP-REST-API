7fc1406858ddd1b55584f24d36341483d084bf42
test <test@gmail.com>
2020-12-07 18:44:09 +0330

    require Response.php and validate_functions_helper.php in system/init.php

--- system/init.php
+++ system/init.php
@@ -3,5 +3,7 @@
 require_once '../vendor/autoload.php';
 require_once '../app/Config/Database.php';
 
-require_once "App.php";
-require_once "Controller.php";
+require_once 'App.php';
+require_once 'Controller.php';
+require_once '../app/Utilities/Response.php';
+require_once '../app/helpers/validate_functions_helper.php';
