9cafd3d2a55f38321a4e1ddab064d24b4c4558dd
test <test@gmail.com>
2020-12-07 18:58:13 +0330

    Completed ApiV1.php

--- app/Controllers/ApiV1.php
+++ app/Controllers/ApiV1.php
@@ -1,9 +1,42 @@
 <?php
 
-class ApiV1
+class ApiV1 extends Controller
 {
-    public function index()
+    public function get($country_id = null, $page = null, $pagesize = null)
     {
-        echo "Hello World!";
+        $model = $this->model('countries_model');
+        $results = $model->get($country_id, $page, $pagesize);
+        $response = [];
+        foreach ($results as $item) {
+            array_push($response, $item);
+        }
+        return $response;
+    }
+
+    public function post($data = [])
+    {
+        if ($data) {
+            $model = $this->model('countries_model');
+            $response = $model->insert($data);
+            return [$response];
+        }
+        return [false];
+    }
+
+    public function put($data = [])
+    {
+        if ($data) {
+            $model = $this->model('countries_model');
+            $response = $model->update_model($data);
+            return [$response];
+        }
+        return [false];
+    }
+
+    public function delete($country_id)
+    {
+        $model = $this->model('countries_model');
+        $result = $model->delete_model($country_id);
+        return [$result];
     }
 }
